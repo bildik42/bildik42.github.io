5e37b869d94c3a7988effc862738e239734e2c5e
b4r5182 <barkod.tedi.ncity.1271@gmail.com>
2024-07-13 18:50:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v2'; // Cache versiyonunu artırdık
+const CACHE_NAME = 'v1';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -10,37 +10,16 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
-    self.skipWaiting(); // Yeni service worker'ın hemen aktif olmasını sağlıyoruz
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim(); // Yeni service worker'ı hemen kullanmaya başlamak için
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                if (response) {
-                    return response; // Cache'de varsa cache'den dönüyoruz
-                }
-                return fetch(event.request); // Cache'de yoksa network'ten alıyoruz
+                return response || fetch(event.request);
             })
     );
 });
